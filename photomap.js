2ddea577dfede4a1aa27ec7ff3a7fee0ad4625d1
erinvoisin-sketch <erinvoisin@gmail.com>
2026-04-19 21:43:17 -0500

    Update photomap.js

--- photomap.js
+++ photomap.js
@@ -23,7 +23,7 @@ function createPhotoMap () {
  var OpenStreetMap_Mapnik = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}) addTo(map);
+}).addTo(map);
   
   // use Papa Parse (papaparse.com) to get the Google Sheets CSV
   Papa.parse(csvUrl, {
